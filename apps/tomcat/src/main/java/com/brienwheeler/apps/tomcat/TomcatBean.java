4b6c54764de8e51ae3a320d6038a8aceb96f85f7
Brien Wheeler <brien@briens-mbp.wp.comcast.net>
2014-07-09 15:34:35 -0400

    Support for SSL in TomcatBean.

--- apps/tomcat/src/main/java/com/brienwheeler/apps/tomcat/TomcatBean.java
+++ apps/tomcat/src/main/java/com/brienwheeler/apps/tomcat/TomcatBean.java
@@ -23,30 +23,52 @@
  */
 package com.brienwheeler.apps.tomcat;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
-import java.security.ProtectionDomain;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfoBuilder;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Required;
 
 public class TomcatBean implements InitializingBean, DisposableBean
 {
-	private final static Log log = LogFactory.getLog(TomcatBean.class);
-	
+	private static final Log log = LogFactory.getLog(TomcatBean.class);
+
+    private static final Pattern KEY_PATTERN = Pattern.compile(
+            "-+BEGIN (.*)PRIVATE KEY-+([^-]*)-+END .*PRIVATE KEY-+");
+    private static final Pattern CERT_PATTERN = Pattern.compile(
+            "-+BEGIN CERTIFICATE-+([^-]*)-+END CERTIFICATE-+");
+
+    private static final String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
 	private int port;
+    private int sslPort;
+    private File sslKeyFile;
+    private File sslCertFile;
 	private String baseDirectory;
 	private String webAppBase = null;
 	private String contextRoot = null;
@@ -62,9 +84,8 @@ public class TomcatBean implements InitializingBean, DisposableBean
 	{
 		tomcat.setBaseDir(baseDirectory);
 		tomcat.getHost().setAppBase(baseDirectory);
-		tomcat.setPort(port);
+        configureNetwork();
 		extractWarFile();
-
 		tomcat.start();
 	}
 
@@ -97,8 +118,66 @@ public class TomcatBean implements InitializingBean, DisposableBean
 	{
 		this.port = port;
 	}
-	
-	private void extractWarFile()
+
+    @Required
+    public void setSslPort(int sslPort)
+    {
+        this.sslPort = sslPort;
+    }
+
+    @Required
+    public void setSslKeyFile(File sslKeyFile)
+    {
+        this.sslKeyFile = sslKeyFile;
+    }
+
+    @Required
+    public void setSslCertFile(File sslCertFile)
+    {
+        this.sslCertFile = sslCertFile;
+    }
+
+    private void configureNetwork() throws Exception
+    {
+        if (port > 0) {
+            tomcat.setPort(port);
+        }
+        else {
+            tomcat.getService().removeConnector(tomcat.getConnector());
+        }
+
+        if (sslPort > 0) {
+            StringBuffer randomPass = new StringBuffer();
+            for (int i=0; i<10; i++)
+                randomPass.append(characters.charAt((int) (characters.length() * Math.random())));
+            String keystorePass = randomPass.toString();
+
+            RSAPrivateKey privateKey = readKeyFile();
+            log.info("successfully read SSL private key from " + sslKeyFile.getAbsolutePath());
+            X509Certificate certificate = readCertFile();
+            log.info("successfully read SSL certificate from " + sslCertFile.getAbsolutePath());
+
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(null);
+            keyStore.setCertificateEntry("cert-alias", certificate);
+            keyStore.setKeyEntry("key-alias", privateKey, keystorePass.toCharArray(), new Certificate[]{certificate});
+            File keyStoreFile = new File("tcks");
+            keyStore.store(new FileOutputStream(keyStoreFile), keystorePass.toCharArray());
+
+            Connector sslConnector = new Connector();
+            sslConnector.setPort(sslPort);
+            sslConnector.setSecure(true);
+            sslConnector.setScheme("https");
+            sslConnector.setAttribute("keystoreFile", keyStoreFile.getAbsolutePath());
+            sslConnector.setAttribute("keystorePass", keystorePass);
+            sslConnector.setAttribute("clientAuth", "false");
+            sslConnector.setAttribute("sslProtocol", "TLS");
+            sslConnector.setAttribute("SSLEnabled", true);
+            tomcat.getService().addConnector(sslConnector);
+        }
+    }
+
+    private void extractWarFile()
 	{
 		if (webAppBase != null && webAppBase.length() > 0) {
 			ProtectionDomain protectionDomain = this.getClass().getProtectionDomain();
@@ -155,6 +234,59 @@ public class TomcatBean implements InitializingBean, DisposableBean
 				throw new RuntimeException("error extracting WAR file", e);
 			}
 		}
-
 	}
+
+    private RSAPrivateKey readKeyFile() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String parse[] = readPEMFile(sslKeyFile, KEY_PATTERN, 2);
+        if (parse == null)
+            throw new IllegalArgumentException("invalid key file contents");
+
+        if (parse[0].length() == 0) { // BEGIN PRIVATE KEY
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPrivateKey) keyFactory.generatePrivate(new PKCS8EncodedKeySpec(Base64.decode(parse[1])));
+        }
+
+        if (parse[0].contains("RSA")) { // BEGIN RSA PRIVATE KEY
+            Security.addProvider(new BouncyCastleProvider());
+
+            PEMParser pemParser = new PEMParser(new FileReader(sslKeyFile));
+            Object parsedObject = pemParser.readObject();
+            if (!(parsedObject instanceof PEMKeyPair))
+                throw new IllegalArgumentException("invalid key file contents");
+
+            PEMKeyPair keyPair = (PEMKeyPair) parsedObject;
+            RSAPrivateKey privateKey = (RSAPrivateKey) BouncyCastleProvider.getPrivateKey(keyPair.getPrivateKeyInfo());
+            if (privateKey == null)
+                throw new IllegalArgumentException("invalid key file contents");
+            return privateKey;
+        }
+
+        throw new IllegalArgumentException("invalid key file contents");
+    }
+
+    private X509Certificate readCertFile() throws IOException, CertificateException {
+        String parse[] = readPEMFile(sslCertFile, CERT_PATTERN, 1);
+        if (parse == null)
+            throw new IllegalArgumentException("invalid certificate file contents");
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        return (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(Base64.decode(parse[0])));
+    }
+
+    private String[] readPEMFile(File inFile, Pattern pattern, int groups) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+
+        BufferedReader reader = new BufferedReader(new FileReader(inFile));
+        for (String line=reader.readLine(); line!=null; line=reader.readLine())
+            buffer.append(line);
+        reader.close();
+
+        Matcher matcher = pattern.matcher(buffer.toString());
+        if (!matcher.matches())
+            return null;
+
+        String[] ret = new String[groups];
+        for (int i=0; i<groups; i++)
+            ret[i] = matcher.group(i + 1);
+        return ret;
+    }
 }
